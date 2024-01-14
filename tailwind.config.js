df260e7c1c242a71e055819f4ba39317fce35e19
skhacker105 <skhacker105@gmail.com>
2024-01-14 16:34:08 +0530

    updated tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,59 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../src/**/*.{html,ts,scss}',
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        "default": '11px',
+        "page-header": '11px',
+
+        "dashboard-box": '9px',
+        "dashboard-title": '9px',
+      },
+      backgroundColor: {
+        "default": 'white',
+        "page-content": 'transparent',
+        "dashboard": {
+          "logo": 'transparent',
+          "box": '#afb1c3c2',
+          "title": '#020268',
+          "title-logo": 'transparent',
+        },
+      },
+      textColor: {
+        "page-header": '#ffffff',
+        "page-footer": '#ffffff',
+        "dashboard": {
+          "box": '#000000',
+          "title": '#ffffff'
+        },
+      },
+      colors: {
+        "dashboard": {
+          "logo": 'black',
+          "title-logo": 'white'
+        },
+      },
+      backgroundImage: {
+        "page-header": `url('assets/bg-header.png')`,
+        "page-footer": `url('assets/bg-footer.png')`,
+        "dashboard-title": `url('assets/bg-footer.png')`,
+      },
+      lineHeight: {
+        "page-header": '33px'
+      },
+      borderColor: {
+        "dashboard": {
+          "title": "white"
+        }
+      }
+    },
+  },
+  plugins: [],
+  safelist: [{
+    pattern: /(bg|background|text|leading|fill|stroke|border-b)-(default|page|dashboard|url)/
+  }]
+}
+
