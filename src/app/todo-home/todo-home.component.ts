a692f45aee9153b6bd5b88868e34b2c2a9d9e61b
Diksha Bharti <dikshab0202@gmail.com>
2024-01-23 00:45:03 +0530

    merge conflicts resolved

--- src/app/todo-home/todo-home.component.ts
+++ src/app/todo-home/todo-home.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
 import { DynamicLayoutComponent } from 'shop-folder-component';
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { GridService, ITodoGroup } from 'shop-folder-core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-todo-home',
   standalone: true,
-  imports: [DynamicLayoutComponent],
+  imports: [DynamicLayoutComponent, MatIconModule],
   templateUrl: './todo-home.component.html',
-  styleUrl: './todo-home.component.scss'
+  styleUrl: './todo-home.component.scss',
 })
-export class TodoHomeComponent {
+export class TodoHomeComponent
+  extends GridService<ITodoGroup>
+  implements OnInit
+{
+  override handleSelectModeOn(): void {
+    throw new Error('Method not implemented.');
+  }
+  override handleSelectModeOff(): void {
+    throw new Error('Method not implemented.');
+  }
 
+  constructor(public route: ActivatedRoute) {
+    super([], route);
+  }
+  ngOnInit(): void {}
 }
